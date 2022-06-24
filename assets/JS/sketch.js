1f558cef3e01aa98469c84504912411b0aa5a5d8
davideformenti <51033437+davideformenti@users.noreply.github.com>
2022-06-24 18:12:39 +0200

    mobile fix layout v3

--- assets/JS/sketch.js
+++ assets/JS/sketch.js
@@ -419,8 +419,8 @@ for (let i = n; i > 0; i--) {
   let size = radius + i * inter;
   let k = kMax * sqrt(i/n);
   if (windowWidth<windowHeight){
-    maxNoise=800;
-    n = 20; // number of blobs
+    maxNoise=700;
+    n = 10; // number of blobs
     radius = 2; // diameter of the circle
     inter = 2; // difference between the sizes of two blobs
   }
@@ -430,8 +430,8 @@ for (let i = n; i > 0; i--) {
   noisiness = maxNoise * noiseProg(i / n);
   water_pg.stroke(SoilHumidity*2,100,SoilHumidity/2);
   if (windowWidth<windowHeight){ //mobile
-  blob(size, water_pg.width/2, water_pg.height, k, t - i * step, noisiness);
-  blob(size, water_pg.width/2, -water_pg.width/4, k, t - i * step, noisiness);
+  blob(size, water_pg.width/2, water_pg.height*1.25, k, t - i * step, noisiness);
+  blob(size, water_pg.width/2, -water_pg.height/6, k, t - i * step, noisiness);
   }
   else{
     blob(size, water_pg.width*1.3, water_pg.height/2, k, t - i * step, noisiness);
